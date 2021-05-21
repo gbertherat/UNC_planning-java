dfc13e69f4f7bd9c0bfac83a7456998e19d139e0
gbertherat <61923651+gbertherat@users.noreply.github.com>
2021-05-21 17:42:14 +1100

    Initial commit

---
+++ src/test/java/nc/univ/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.univ.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
